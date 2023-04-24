7900a14c3a3887d292d2843a6a7e5c9973375c51
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2023-04-24 16:28:21 +0500

    1 file modified

--- MSF.tsx
+++ MSF.tsx
@@ -1,16 +1,29 @@
 import { ReactElement, useState } from 'react';
 export default function (steps: ReactElement[]) {
   const [currentStepIndex, setCurrentSetupIndex] = useState(0);
-  function next() {}
-  function back() {}
+  function next() {
+    setCurrentSetupIndex(i=>{
+      if(i>=steps.length-1)return i
+      return i+1
+    })
+  }
+  function back() {
+    setCurrentSetupIndex(i=>{
+      if(i<=0)return i
+      return i-1
+    })
+  }
   function goTo(index:number){
     setCurrentSetupIndex(index)
 
   }
 
   return{
+  
+
   currentStepIndex,
   step: steps[currentStepIndex],
   goTo
+  
 }
 }
