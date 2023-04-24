38882fcb3536edb992380c3f891089975aeb5aa0
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2023-04-24 16:38:50 +0500

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
-
 export default function App() {
   return (
-    <div>
-      
+    <div style={{position:"relative", background:"white",
+    border: "solid 1px black",
+    padding:"2rem",margin:"1rem", borderRadius:".5rem",
+    fontFamily:"Arial"   }}>
+      <form action="">
+        <div style={{position:"absolute"
+      ,top:".5rem", right:".5rem"}}>  0/2
+
+        </div>
+      </form>
     </div>
   );
 }
--- MSF.tsx
+++ MSF.tsx
@@ -2,28 +2,26 @@ import { ReactElement, useState } from 'react';
 export default function (steps: ReactElement[]) {
   const [currentStepIndex, setCurrentSetupIndex] = useState(0);
   function next() {
-    setCurrentSetupIndex(i=>{
-      if(i>=steps.length-1)return i
-      return i+1
-    })
+    setCurrentSetupIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
   }
   function back() {
-    setCurrentSetupIndex(i=>{
-      if(i<=0)return i
-      return i-1
-    })
+    setCurrentSetupIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
   }
-  function goTo(index:number){
-    setCurrentSetupIndex(index)
-
+  function goTo(index: number) {
+    setCurrentSetupIndex(index);
   }
 
-  return{
-  
-
-  currentStepIndex,
-  step: steps[currentStepIndex],
-  goTo
-  
-}
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    goTo,
+    next,
+    back,
+  };
 }
